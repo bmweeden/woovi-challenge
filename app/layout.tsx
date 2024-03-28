2b3552a4b1156678ed9d74c666f156133af4ed7c
Ben Weeden <ben@benweeden.com>
2024-03-28 16:03:48 +0000

    fix: add maximum container size

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,19 @@ export default function RootLayout({
         <head></head>
         <ThemeWrapper>
           <body>
-            <Stack gap={3.75} py={3} px={2}>
-              <Header />
-              <Box component="main">{children}</Box>
-              <Footer />
+            <Stack alignItems="center" width="100%">
+              <Stack
+                justifyContent="center"
+                width="100%"
+                gap={3.75}
+                py={3}
+                px={2}
+                maxWidth={500}
+              >
+                <Header />
+                <Box component="main">{children}</Box>
+                <Footer />
+              </Stack>
             </Stack>
           </body>
         </ThemeWrapper>
